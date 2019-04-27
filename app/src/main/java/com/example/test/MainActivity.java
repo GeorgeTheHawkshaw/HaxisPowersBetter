8030b877c2c494db76a8ba721395639d4d78cb5b
Nathan Heindl <nathanheindl@att.net>
2019-04-27 18:41:13 -0500

    Added some working buttons.

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void buttonOne(View view){
+        b1 button = b1.makeText(this, "IT WORKED", b1.LENGTH_SHORT);
+        b1.show();
+    }
+    public void test(View view){
+        TextView countView = (TextView)findViewById(R.id.textView);
+
+        String countString = countView.getText().toString();
+
+        Integer count = Integet.parseInt(countString);
+        count++;
+
+        countView.setText(count.toString);
     }
 }
