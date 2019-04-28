fd3aa94d6994a3c8b480e0107506d8189d6f17e8
Nathan Heindl <nathanheindl@att.net>
2019-04-27 22:32:34 -0500

    Added the list for the appliances.

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -4,17 +4,23 @@ import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Spinner;
+import java.util.*;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    List<String> appliance_names = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     public void buttonOne(View view){
         Toast b1 = Toast.makeText(this, "IT WORKED", Toast.LENGTH_SHORT);
@@ -32,8 +38,25 @@ public class MainActivity extends AppCompatActivity {
     }
     public void getIT(View view){
         EditText content = (EditText)findViewById(R.id.EditText_1);
-        TextView text = (TextView)findViewById(R.id.textView2);
+
         String result = content.getText().toString();
-        text.setText(result);
+        appliance_names.add(result);
+
+        final Spinner spin = (Spinner) findViewById(R.id.spinner2);
+        spin.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, appliance_names);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(dataAdapter);
+
+
+        //Spinner spin = (Spinner) findViewById(R.id.spinner2);
+
+
+        //TextView text = (TextView)findViewById(R.id.textView2);
+        //text.setText(result);
     }
+
 }
